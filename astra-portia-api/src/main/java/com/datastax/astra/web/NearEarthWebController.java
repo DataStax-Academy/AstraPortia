59c6fa9f780743d8bff3063cb2fa549d5b96e314
clunven <cedrick.lunven@gmail.com>
2020-09-11 18:38:17 +0200

    new controllers

---
+++ astra-portia-api/src/main/java/com/datastax/astra/web/NearEarthWebController.java
@@ -0,0 +1,73 @@
+package com.datastax.astra.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.context.WebContext;
+
+import com.datastax.astra.nearearthobject.NearEarthObject;
+import com.datastax.astra.web.model.NearEarthObjectWebBean;
+
+@Controller
+@RequestMapping(value="/nearearth")
+public class NearEarthWebController  extends AbstractController {
+    
+    /** Vie name. */
+    private static final String HOME_NEO = "nearearth";
+
+    /** {@inheritDoc} */
+    @Override
+    public String getSuccessView() {
+        return HOME_NEO;
+    }
+    
+    /** {@inheritDoc} */
+    @Override
+    public void get(HttpServletRequest req, HttpServletResponse res, WebContext ctx) 
+    throws Exception {
+        List<NearEarthObject> nearEarthObjects = new ArrayList();
+        NearEarthObject o1 =  new NearEarthObject();
+        o1.setDesignation("o1.designation");
+        o1.setDiscoveryDate("10/09/2020");
+        o1.setH_mag("12.2");
+        o1.setI_deg("13.3");
+        o1.setMoid_au("3.0");
+        o1.setOrbitClass("PLUTON");
+        o1.setPeriod_yr(3.4);
+        o1.setPha("Y");
+        o1.setQ_au_1(1.2);
+        o1.setQ_au_1(2.1);
+        
+        NearEarthObject o2 =  new NearEarthObject();
+        o2.setDesignation("o2.designation");
+        o2.setDiscoveryDate("21/07/2020");
+        o2.setH_mag("12.2");
+        o2.setI_deg("13.3");
+        o2.setMoid_au("3.0");
+        o2.setOrbitClass("PLUTON");
+        o2.setPeriod_yr(3.4);
+        o2.setPha("Y");
+        o2.setQ_au_1(1.2);
+        o2.setQ_au_1(2.1);
+        
+        nearEarthObjects.add(o1);
+        nearEarthObjects.add(o2);
+        NearEarthObjectWebBean web = new NearEarthObjectWebBean();
+        web.setNeo(nearEarthObjects);
+        
+        ctx.setVariable("cbean", web);
+        
+    }
+    
+    /** {@inheritDoc} */
+    @Override
+    public void processPost(HttpServletRequest req, HttpServletResponse res, WebContext ctx) 
+    throws Exception {}
+
+    
+}
