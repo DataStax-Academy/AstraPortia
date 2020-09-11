cf6368d2f524d2b01d18abad0e23aac97720719a
bruno <guedes.bruno@gmail.com>
2020-09-11 18:06:27 +0200

    compile issue - correction done

--- astra-portia-api/src/main/java/com/datastax/astra/nearearthobject/NearEarthObjectController.java
+++ astra-portia-api/src/main/java/com/datastax/astra/nearearthobject/NearEarthObjectController.java
@@ -42,17 +42,20 @@ import java.net.URISyntaxException;
 @RequestMapping("/api/earths")
 @Tag(name = "AstraPortia", description = "sample")
 public class NearEarthObjectController {
-    
-    /** Logger for the class. */
+
+    /**
+     * Logger for the class.
+     */
     private static final Logger logger = LoggerFactory.getLogger(NearEarthObjectController.class);
-    
+
     @Autowired
     private AstraStargateApiClient stargateClient;
-    
+
     private String authToken;
-    
-    public NearEarthObjectController() {}
-    
+
+    public NearEarthObjectController() {
+    }
+
     public NearEarthObjectController(AstraStargateApiClient stargateClient) {
         this.stargateClient = stargateClient;
         Optional<String> token = stargateClient.authentiticate();
@@ -61,32 +64,32 @@ public class NearEarthObjectController {
             logger.info("Your are authenticated to Astra with {}", authToken);
         }
     }
-     
+
     /**
      * Retrieve all tasks (GET)
      */
     @Operation(
             summary = "Retrieve an object",
             description = "retrieve an Object",
-            tags = { "findById" })
+            tags = {"findById"})
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200",
-                         description = "Sample",
-             content = @Content(schema = @Schema(implementation = NearEarthObject.class))),
+                    description = "Sample",
+                    content = @Content(schema = @Schema(implementation = NearEarthObject.class))),
             @ApiResponse(responseCode = "400", description = "Title is blank but is mandatory"),
-            @ApiResponse(responseCode = "500", description = "An error occur in storage") })
+            @ApiResponse(responseCode = "500", description = "An error occur in storage")})
     @RequestMapping(
             value = "/earth/{earthId}",
             method = GET,
             produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<NearEarthObject> giveMeOneObject(HttpServletRequest request,
-                                                           @Parameter(name="taskId",
-                                                                   description="Unique identifier for earth to be retreived",
+                                                           @Parameter(name = "taskId",
+                                                                   description = "Unique identifier for earth to be retreived",
                                                                    example = "Kepler-251 d",
-                                                                   required=true )
+                                                                   required = true)
                                                            @PathVariable(value = "earthId") String taskId) {
         logger.info("get an object");
-        NearEarthObject o1 =  new NearEarthObject();
+        NearEarthObject o1 = new NearEarthObject();
         return ResponseEntity.ok(o1);
     }
 
@@ -96,74 +99,75 @@ public class NearEarthObjectController {
     @Operation(
             summary = "Retrieve all the nearest earth",
             description = "retrieve all nearest earth",
-            tags = { "earths" })
+            tags = {"earths"})
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200",
                     description = "Sample",
                     content = @Content(schema = @Schema(implementation = NearEarthObject.class))),
             @ApiResponse(responseCode = "400", description = "Title is blank but is mandatory"),
-            @ApiResponse(responseCode = "500", description = "An error occur in storage") })
+            @ApiResponse(responseCode = "500", description = "An error occur in storage")})
     @RequestMapping(
             value = "/",
             method = GET,
             produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<List<NearEarthObject>> findAll(HttpServletRequest request) {
         logger.info("find all NearEarth documents");
-        
+
         List<NearEarthObject> nearEarthObjects = new ArrayList();
-        
-        NearEarthObject o1 =  new NearEarthObject();
+
+        NearEarthObject o1 = new NearEarthObject();
         o1.setDesignation("o1.designation");
         o1.setDiscoveryDate("10/09/2020");
-        
-        NearEarthObject o2 =  new NearEarthObject();
+
+        NearEarthObject o2 = new NearEarthObject();
         o2.setDesignation("o2.designation");
         o2.setDiscoveryDate("21/07/2020");
-        
+
         nearEarthObjects.add(o1);
         nearEarthObjects.add(o2);
-        
+
         return ResponseEntity.ok(nearEarthObjects);
     }
 
     @Operation(
             summary = "Update an existing earth",
             description = "Update a Earth document passing id and providing body",
-            tags = { "update" })
+            tags = {"update"})
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successful operation",
-                    content = @Content(schema = @Schema(implementation = Todo.class))),
+                    content = @Content(schema = @Schema(implementation = NearEarthObject.class))),
             @ApiResponse(responseCode = "400", description = "Json body not valid"),
-            @ApiResponse(responseCode = "404", description = "Task UUID not found") })
+            @ApiResponse(responseCode = "404", description = "Task UUID not found")})
     @RequestMapping(
             value = "/{taskId}",
             method = PATCH,
             produces = APPLICATION_JSON_VALUE,
             consumes = APPLICATION_JSON_VALUE)
     public ResponseEntity<NearEarthObject> update(HttpServletRequest request,
-                                       @Parameter(name="earthId", required=true,
-                                               description="Unique identifier for a earth",
-                                               example = "Kepler-251 d")
-                                       @PathVariable(value = "earthId") String taskId,
-                                       @RequestBody
-                                       @io.swagger.v3.oas.annotations.parameters.RequestBody(
-                                               description = "Update all fields if needed",
-                                               required = true,
-                                               content = @Content(schema = @Schema(implementation = Todo.class)))
-                                               Todo task)
+                                                  @Parameter(name = "earthId", required = true,
+                                                          description = "Unique identifier for a earth",
+                                                          example = "Kepler-251 d")
+                                                  @PathVariable(value = "earthId") String taskId,
+                                                  @RequestBody
+                                                      @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                                                              description = "Update all fields if needed",
+                                                              required = true,
+                                                              content = @Content(schema = @Schema(implementation = NearEarthObject.class)))
+                                                              NearEarthObject earth)
             throws URISyntaxException {
         logger.info("update/modify a earth");
-        NearEarthObject o1 =  new NearEarthObject();
+        NearEarthObject o1 = new NearEarthObject();
         return ResponseEntity.ok(o1);
     }
 
     @RequestMapping(value = "/{earthId}", method = DELETE)
     @Operation(summary = "Delete a Earth from its id of exists", description = "Delete a earth from its id of exists")
-    @ApiResponses(value = { @ApiResponse(responseCode = "204", description = "No results") } )
+    @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "No results")})
     public ResponseEntity<Void> delete(HttpServletRequest request,
-                                       @Parameter(name="earthId", required=true,
-                                               description="Unique identifier for the earth",
+                                       @Parameter(name = "earthId", required = true,
+                                               description = "Unique identifier for the earth",
                                                example = "Kepler-251 d")
                                        @PathVariable(value = "earthId") String earthId) {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+}
