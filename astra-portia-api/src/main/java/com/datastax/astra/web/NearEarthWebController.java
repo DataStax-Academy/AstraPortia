513a832789ad3f2c1232e49f2761c9f97190ac33
clunven <cedrick.lunven@gmail.com>
2020-09-11 19:31:53 +0200

    list

--- astra-portia-api/src/main/java/com/datastax/astra/web/NearEarthWebController.java
+++ astra-portia-api/src/main/java/com/datastax/astra/web/NearEarthWebController.java
@@ -32,31 +32,44 @@ public class NearEarthWebController  extends AbstractController {
     throws Exception {
         List<NearEarthObject> nearEarthObjects = new ArrayList();
         NearEarthObject o1 =  new NearEarthObject();
-        o1.setDesignation("o1.designation");
+        o1.setDesignation("Apophis");
         o1.setDiscoveryDate("10/09/2020");
         o1.setH_mag("12.2");
         o1.setI_deg("13.3");
         o1.setMoid_au("3.0");
-        o1.setOrbitClass("PLUTON");
+        o1.setOrbitClass("Asteroid");
         o1.setPeriod_yr(3.4);
         o1.setPha("Y");
         o1.setQ_au_1(1.2);
         o1.setQ_au_1(2.1);
         
         NearEarthObject o2 =  new NearEarthObject();
-        o2.setDesignation("o2.designation");
+        o2.setDesignation("Pluto");
         o2.setDiscoveryDate("21/07/2020");
         o2.setH_mag("12.2");
         o2.setI_deg("13.3");
         o2.setMoid_au("3.0");
-        o2.setOrbitClass("PLUTON");
+        o2.setOrbitClass("Teluric");
         o2.setPeriod_yr(3.4);
         o2.setPha("Y");
         o2.setQ_au_1(1.2);
         o2.setQ_au_1(2.1);
         
+        NearEarthObject o3 =  new NearEarthObject();
+        o3.setDesignation("Mars");
+        o3.setDiscoveryDate("21/07/2020");
+        o3.setH_mag("12.2");
+        o3.setI_deg("13.3");
+        o3.setMoid_au("3.0");
+        o3.setOrbitClass("Teluric");
+        o3.setPeriod_yr(3.4);
+        o3.setPha("Y");
+        o3.setQ_au_1(1.2);
+        o3.setQ_au_1(2.1);
+        
         nearEarthObjects.add(o1);
         nearEarthObjects.add(o2);
+        nearEarthObjects.add(o3);
         NearEarthObjectWebBean web = new NearEarthObjectWebBean();
         web.setNeo(nearEarthObjects);
         
